e380b312cf7545f91990eb8c88831425b29c55c3
ravenlight17 <cbas1117@gmail.com>
2022-07-13 19:19:20 -0400

    js fetch init & css config

---
+++ main.js
@@ -0,0 +1,9 @@
+let api_key = document.getElementById('api_key');
+
+let title_input = document.getElementById('title');
+
+let year_input = document.getElementById('year');
+
+let url = 'http://www.omdbapi.com/?i=tt3896198&apikey=';
+
+fetch(url`${api_key}`);
