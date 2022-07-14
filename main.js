44b8292319a7a1daf1d07bd73b28a32eddb0808c
ravenlight17 <cbas1117@gmail.com>
2022-07-14 15:04:35 -0400

    3 files modified

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-let api_key = document.getElementById('api_key');
-let value = api_key.getAttribute('name');
-console.log(value);
 
-let title_input = document.getElementById('title');
 
-let year_input = document.getElementById('year');
 
-let url = 'http://www.omdbapi.com/?i=tt3896198&apikey=';
 
-let btn = document.getElementById('btn');
 
-btn.addEventListener('click', function () {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => console.log(err));
-});
+
+
+
+
+// let api_key = document.getElementById('api_key');
+// let value = api_key.getAttribute('name');
+// console.log(value);
+
+// let title_input = document.getElementById('title');
+
+// let year_input = document.getElementById('year');
+
+// let form = document.getElementById('form-movie');
+
+// let url = 'http://www.omdbapi.com/?i=tt3896198&apikey=';
+
+// let btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function () {
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch((err) => console.log(err));
+// });
